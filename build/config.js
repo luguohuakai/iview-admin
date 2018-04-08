18bc53ce36610d7459020ea31f71f084b01d0060
DM <769245396@qq.com>
2018-04-08 15:35:44 +0800

    新增 测试接口

--- build/config.js
+++ build/config.js
@@ -1,7 +1,12 @@
 import Env from './env';
 
+console.error(Env);
+
+let apis_prefix = Env === 'production' ? 'https://api.srun.com/' : 'http://106.14.7.51/';
+
+console.error(apis_prefix);
 let config = {
     env: Env,
-    api: 'https://api.srun.com/'
+    api: apis_prefix
 };
 export default config;
