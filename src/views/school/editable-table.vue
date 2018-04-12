476d3c8962df88ec55a54e3c5cee3ceae1211585
DM <769245396@qq.com>
2018-04-12 18:00:38 +0800

    --

--- src/views/school/editable-table.vue
+++ src/views/school/editable-table.vue
@@ -1,43 +1,6 @@
 <style lang="less">
     @import '../../styles/common.less';
     @import './components/table.less';
-
-    .demo-upload-list{
-        display: inline-block;
-        width: 60px;
-        height: 60px;
-        text-align: center;
-        line-height: 60px;
-        border: 1px solid transparent;
-        border-radius: 4px;
-        overflow: hidden;
-        background: #fff;
-        position: relative;
-        box-shadow: 0 1px 1px rgba(0,0,0,.2);
-        margin-right: 4px;
-    }
-    .demo-upload-list img{
-        width: 100%;
-        height: 100%;
-    }
-    .demo-upload-list-cover{
-        display: none;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background: rgba(0,0,0,.6);
-    }
-    .demo-upload-list:hover .demo-upload-list-cover{
-        display: block;
-    }
-    .demo-upload-list-cover i{
-        color: #fff;
-        font-size: 20px;
-        cursor: pointer;
-        margin: 0 2px;
-    }
 </style>
 
 <template>
@@ -113,44 +76,17 @@
 
 
                 <FormItem label="颜色" prop="color">
-                    <Input v-model="formItem.color" placeholder="请输入颜色"></Input>
+                    <ColorPicker v-model="formItem.color"/>
                 </FormItem>
-
-
 
                 <FormItem label="Logo" prop="logo">
-                    <div class="demo-upload-list" v-for="item in uploadList">
-                        <template v-if="item.status === 'finished'">
-                            <img :src="item.url">
-                            <div class="demo-upload-list-cover">
-                                <Icon type="ios-eye-outline" @click.native="handleView(item.name)"></Icon>
-                                <Icon type="ios-trash-outline" @click.native="handleRemove(item)"></Icon>
-                            </div>
-                        </template>
-                        <template v-else>
-                            <Progress v-if="item.showProgress" :percent="item.percentage" hide-info></Progress>
-                        </template>
-                    </div>
-                    <input v-if="formItem.logo" v-model="formItem.logo" type="hidden" name="logo">
                     <Upload
-                            ref="upload"
-                            :show-upload-list="false"
-                            :default-file-list="defaultList"
-                            :on-success="handleSuccess"
-                            :format="['jpg','jpeg','png']"
-                            :max-size="2048"
-                            :on-format-error="handleFormatError"
-                            :on-exceeded-size="handleMaxSize"
-                            :before-upload="handleBeforeUpload"
-                            multiple
-                            type="drag"
-                            action="//jsonplaceholder.typicode.com/posts/"
-                            style="display: inline-block;width:58px;">
-                        <div style="width: 58px;height:58px;line-height: 58px;">
-                            <Icon type="camera" size="20"></Icon>
-                        </div>
+                            :before-upload="handleUpload"
+                            action="//jsonplaceholder.typicode.com/posts/">
+                        <Button type="ghost" icon="ios-cloud-upload-outline">选择图片</Button>
                     </Upload>
                 </FormItem>
+
 
 
             </Form>
@@ -160,9 +96,6 @@
             </div>
         </Modal>
 
-        <Modal title="View Image" v-model="visible">
-            <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%">
-        </Modal>
     </div>
 </template>
 
@@ -189,33 +122,19 @@
           index: 0
         },
         formItem: {
-          apiClass: '',
-          info: '',
-          groupHash: 'default',
-          method: 2,
-          hash: '',
-          accessToken: 1,
-          isTest: 0,
-          needLogin: 0,
+          name: '',
+          addr: '',
+          lat: '',
+          lng: '',
+          color: '',
+          logo: '',
+          login_bg: '',
           id: 0
         },
         formInline: {
           name: '',
           addr: ''
-        },
-        defaultList: [
-          {
-            'name': 'a42bdcc1178e62b4694c830f028db5c0',
-            'url': 'https://o5wwk8baw.qnssl.com/a42bdcc1178e62b4694c830f028db5c0/avatar'
-          },
-          {
-            'name': 'bc7521e033abdd1e92222d733590f104',
-            'url': 'https://o5wwk8baw.qnssl.com/bc7521e033abdd1e92222d733590f104/avatar'
-          }
-        ],
-        imgName: '',
-        visible: false,
-        uploadList: []
+        }
       };
     },
     methods: {
@@ -348,6 +267,10 @@
       cancel () {
         this.modalSetting.show = false;
       },
+      handleUpload (file) {
+        this.file = file;
+        return false;
+      },
       submit () {
         let self = this;
         this.$refs['myForm'].validate((valid) => {
@@ -370,47 +293,11 @@
             });
           }
         });
-      },
-      handleView (name) {
-        this.imgName = name;
-        this.visible = true;
-      },
-      handleRemove (file) {
-        const fileList = this.$refs.upload.fileList;
-        this.$refs.upload.fileList.splice(fileList.indexOf(file), 1);
-      },
-      handleSuccess (res, file) {
-        file.url = 'https://o5wwk8baw.qnssl.com/7eb99afb9d5f317c912f08b5212fd69a/avatar';
-        file.name = '7eb99afb9d5f317c912f08b5212fd69a';
-      },
-      handleFormatError (file) {
-        this.$Notice.warning({
-          title: 'The file format is incorrect',
-          desc: 'File format of ' + file.name + ' is incorrect, please select jpg or png.'
-        });
-      },
-      handleMaxSize (file) {
-        this.$Notice.warning({
-          title: 'Exceeding file size limit',
-          desc: 'File  ' + file.name + ' is too large, no more than 2M.'
-        });
-      },
-      handleBeforeUpload () {
-        const check = this.uploadList.length < 5;
-        if (!check) {
-          this.$Notice.warning({
-            title: 'Up to five pictures can be uploaded.'
-          });
-        }
-        return check;
       }
     },
     created () {
       this.setColumns();
       this.setData();
-    },
-    mounted () {
-      this.uploadList = this.$refs.upload.fileList;
     }
   };
 
